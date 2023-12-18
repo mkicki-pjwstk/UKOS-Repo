2851ebfd5618c6899658324042f0524936b53e90
Maksymilian_Kicki <maksymilian.kicki@pjwstk.edu.pl>
2023-12-18 09:34:57 -0500

    New feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
+    srand(time(NULL));
     cout << "Hello World!";
     cout << "How you doin'?";
+    cout << "Random number: " << rand();
     return 0;
 }
